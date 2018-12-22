45091387a02b94a2f44cdb7e780c03f9f303721b
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 18:57:49 +0000

    Changed header title

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
   <div class="row">
     <div class="col-md-8 blog-main">
     <h3 class="pb-3 mb-4 font-italic border-bottom">
-        From the Firehose
+        The latest
       </h3>
 
         <?php

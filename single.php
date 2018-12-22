242da514ece955178a755b3790bc4032c96fe4cd
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 18:54:05 +0000

    Added single content pages

---
+++ single.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<main role="main" class="container">
+  <div class="row">
+    <div class="col-md-12 blog-main">
+        <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+        
+            get_template_part( 'content-single', get_post_format() );
+
+        endwhile; endif;
+        ?>
+
+    </div><!-- /.blog-main -->
+
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
+<?php get_footer(); ?>

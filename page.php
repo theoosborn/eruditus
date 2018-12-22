4faef8f1333ed8312930d293fbd620b56beefb4d
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 18:33:58 +0000

    Added page.php

---
+++ page.php
@@ -0,0 +1,19 @@
+<main role="main" class="container">
+  <div class="row">
+    <div class="col-md-12 blog-main">
+        <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+        
+            get_template_part( 'content', get_post_format() );
+
+        endwhile; endif;
+        ?>
+
+    </div><!-- /.blog-main -->
+
+    <?php get_sidebar(); ?>
+
+  </div><!-- /.row -->
+
+</main><!-- /.container -->
+<?php get_footer(); ?>

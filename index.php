f894042804ce805aa3e49b3495e10bb7f2e71963
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 17:59:24 +0000

    Created dynamic posts

--- index.php
+++ index.php
@@ -43,8 +43,13 @@
 <main role="main" class="container">
   <div class="row">
     <div class="col-md-8 blog-main">
+        <?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+        
+            get_template_part( 'content', get_post_format() );
 
-        <?php get_template_part( 'content', get_post_format() ); ?>
+        endwhile; endif;
+        ?>
 
       <nav class="blog-pagination">
         <a class="btn btn-outline-primary" href="#">Older</a>

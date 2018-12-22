d224e54c304486a6be2413f8cdd0939ffe78908a
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 19:18:28 +0000

    Revert "Made author's name dark"
    This reverts commit 0b3c3d557fc6a2cf091288a78b853331d45be47c.

--- content-single.php
+++ content-single.php
@@ -1,5 +1,5 @@
 <div class="blog-post">
         <h2 class="blog-post-title"><?php the_title(); ?></h2>
-        <p class="blog-post-meta"><?php the_date(); ?> by <a class="text-dark" href="#"><?php the_author(); ?></a></p>
+        <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
             <?php the_content(); ?>
       </div><!-- /.blog-post -->

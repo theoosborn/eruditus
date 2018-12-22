0b3c3d557fc6a2cf091288a78b853331d45be47c
Theo Osborn <theo.osborn2@gmail.com>
2018-12-22 19:17:37 +0000

    Made author's name dark

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
       <div class="blog-post">
         <h2 class="blog-post-title"><a class="text-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+        <p class="blog-post-meta"><?php the_date(); ?> by <a class="text-dark" href="#"><?php the_author(); ?></a></p>
             <?php the_excerpt(); ?>
       </div><!-- /.blog-post -->

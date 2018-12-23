5631e71c4e6eacd08d8ac9934ae80d41753872a7
Theo Osborn <theo.osborn2@gmail.com>
2018-12-23 19:59:05 +0000

    Added featured image support

--- content-single.php
+++ content-single.php
@@ -1,5 +1,8 @@
 <div class="blog-post">
         <h2 class="blog-post-title"><?php the_title(); ?></h2>
         <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-            <?php the_content(); ?>
+        <?php if ( has_post_thumbnail() ) {
+            the_post_thumbnail();
+        }
+        the_content(); ?>
       </div><!-- /.blog-post -->
--- functions.php
+++ functions.php
@@ -61,3 +61,6 @@
     }
         
     add_action( 'admin_init', 'custom_settings_page_setup' );
+
+    // Support Featured Images
+    add_theme_support( 'post-thumbnails' );

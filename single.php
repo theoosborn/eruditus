6f8947f05558233d51e343641571743ae8ef354e
Theo Osborn <theo.osborn2@gmail.com>
2018-12-23 18:54:08 +0000

    Added comments

--- single.php
+++ single.php
@@ -7,6 +7,10 @@
         
             get_template_part( 'content-single', get_post_format() );
 
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
+
         endwhile; endif;
         ?>
 

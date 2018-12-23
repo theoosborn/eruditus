947c087e67d4dbf8804c3e9c703c9c375462ba5e
Theo Osborn <theo.osborn2@gmail.com>
2018-12-23 19:12:28 +0000

    Changed author

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Theo Osborn">
     <meta name="generator" content="Jekyll v3.8.5">
     <title><?php echo get_bloginfo( 'name' ); ?></title>
 

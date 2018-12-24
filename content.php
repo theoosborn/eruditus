2be14105f5e0330070dce758d75618262cbd3ce8
Theo Osborn <theo.osborn2@gmail.com>
2018-12-24 13:33:39 +0000

    Added thumbnails to index

--- content.php
+++ content.php
@@ -5,5 +5,16 @@
                   printf( _nx( 'One Comment', '%1$s Comments', get_comments_number(), 'comments title', 'textdomain' ), number_format_i18n( get_comments_number() ) ); ?>
                   </a>
             </p>
-            <?php the_excerpt(); ?>
+            <?php if ( has_post_thumbnail() ) {?>
+            <div class="row">
+                  <div class="col-md-4">
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                  </div>
+                  <div class="col-md-6">
+                        <?php the_excerpt(); ?>
+                  </div>
+            </div>
+            <?php } else { 
+                  the_excerpt();
+                  } ?>
       </div><!-- /.blog-post -->
